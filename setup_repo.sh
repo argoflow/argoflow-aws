8843129caf0acc460b69f01347bcc3c4eefb744c
karl.schriek <kschriek@gmail.com>
2021-05-21 13:36:18 +0200

    fix typos

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Perform a simple recursice finad-and-replace on all variables defined in setup.cov
+# Perform a simple recursive find-and-replace on all variables defined in setup.conf
 export SETUP_CONF_PATH=setup.conf # location of the setup config
-export DISTRIBUTION_PATH=./distribution # folder where the distributions YAML files are to be found
+export DISTRIBUTION_PATH=./distribution # folder where the distribution's YAML files are to be found
 
-while IFS="=" read PLACEHOLDER VALUE # While look that will perform simple parsing. On each line MY_VAR=123 will be read into PLACEHOLDER=MY_VAR, VALUE=123
+while IFS="=" read PLACEHOLDER VALUE # While loop that will perform simple parsing. On each line MY_VAR=123 will be read into PLACEHOLDER=MY_VAR, VALUE=123
 do
   # recursively look for $PLACEHOLDER in all files in the $DISTRIBUTION_PATH and replace it with $VALUE
-  VALUE=$(echo "${VALUE////$'\/'}") #escape forward slashes (need for sed to work correctly)
+  VALUE=$(echo "${VALUE////$'\/'}") #escape forward slashes (needed for sed to work correctly)
   grep -rli ${PLACEHOLDER} ${DISTRIBUTION_PATH}/* | xargs -i@ sed -i "s/${PLACEHOLDER}/${VALUE}/g" @ #perform recursive replace
-done <${SETUP_CONF_PATH} # pass the setup config into the while look
+done <${SETUP_CONF_PATH} # pass the setup config into the while loop
