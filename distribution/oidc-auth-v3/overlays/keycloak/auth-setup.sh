d3ab5565a19f14f6c273ca15d328a75a3cb5ca43
DavidSpek <vanderspek.david@gmail.com>
2021-05-24 17:22:16 +0200

    Finish Keycloak implementation

--- distribution/oidc-auth-v3/overlays/keycloak/auth-setup.sh
+++ distribution/oidc-auth-v3/overlays/keycloak/auth-setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 COOKIE_SECRET=$(python3 -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(16)).decode())')
-OIDC_CLIENT_ID="oauth2-proxy"
-OIDC_CLIENT_SECRET="129789cf-c0b6-466b-87fa-512a60b2fd9a"
+OIDC_CLIENT_ID=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+OIDC_CLIENT_SECRET=$(python3 -c 'import secrets; print(secrets.token_hex(32))')
 
 kubectl create secret generic -n auth oauth2-proxy --from-literal=client-id=${OIDC_CLIENT_ID} --from-literal=client-secret=${OIDC_CLIENT_SECRET} --from-literal=cookie-secret=${COOKIE_SECRET} --dry-run=client -o yaml | kubeseal | yq eval -P > oauth2-proxy-secret.yaml
 
-# read -p 'Email: ' EMAIL
-# read -p 'Username: ' USERNAME
+DATABASE_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+POSTGRESQL_PASS=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
+read -p 'Keycloak Admin Password (not for Kubeflow sign in): ' KEYCLOAK_ADMIN_PASS
+read -p 'Keycloak Management Password (not for Kubeflow sign in): ' KEYCLOAK_MANAGEMENT_PASS
 
-# ADMIN_PASS=$(python3 -c 'from passlib.hash import bcrypt; import getpass; print(bcrypt.using(rounds=12, ident="2y").hash(getpass.getpass()))')
+kubectl create secret generic -n auth keycloak-secret --from-literal=admin-password=${KEYCLOAK_ADMIN_PASS} --from-literal=database-password=${DATABASE_PASS} --from-literal=management-password=${KEYCLOAK_MANAGEMENT_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > keycloak-secret.yaml
 
-# read -p 'GitLab Client ID: ' GITLAB_ID
-# read -p 'GitLab Client Secret: ' GITLAB_SECRET
 
-# yq eval ".staticClients[0].id = \"${OIDC_CLIENT_ID}\" | .staticClients[0].secret = \"${OIDC_CLIENT_SECRET}\" | .staticPasswords[0].hash = \"${ADMIN_PASS}\" | .staticPasswords[0].email = \"${EMAIL}\" | .staticPasswords[0].username = \"${USERNAME}\" | .connectors[0].config.clientID = \"${GITLAB_ID}\" | .connectors[0].config.clientSecret = \"${GITLAB_SECRET}\"" dex-config-template.yaml | kubectl create secret generic -n auth dex-config --dry-run=client --from-file=config.yaml=/dev/stdin -o yaml | kubeseal | yq eval -P > dex-config-secret.yaml
+
+kubectl create secret generic -n auth keycloak-postgresql --from-literal=postgresql-password=${DATABASE_PASS} --from-literal=postgresql-postgres-password=${POSTGRESQL_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > postgresql-secret.yaml
+
+read -p 'Email (for Kubeflow login): ' EMAIL
+read -p 'Username (for Kubeflow login): ' USERNAME
+read -p 'Password (for Kubeflow login): ' ADMIN_PASS
+
+yq eval -j -P ".users[0].username = \"${USERNAME}\" | .users[0].email = \"${EMAIL}\" | .users[0].credentials[0].value = \"${ADMIN_PASS}\" | .clients[0].clientId = \"${OIDC_CLIENT_ID}\" | .clients[0].secret = \"${OIDC_CLIENT_SECRET}\"" kubeflow-realm-template.json | kubectl create secret generic -n auth kubeflow-realm --dry-run=client --from-file=kubeflow-realm.json=/dev/stdin -o json | kubeseal | yq eval -P > kubeflow-realm-secret.yaml
