c7f0dd997ec213c94d457c90882eefc244bcb275
DavidSpek <vanderspek.david@gmail.com>
2021-06-02 11:51:23 +0200

    Fix monitoring setup

--- distribution/monitoring-resources/auth_setup.sh
+++
@@ -1,4 +0,0 @@
-read -p 'Username: ' USERNAME
-read -p 'Password: ' ADMIN_PASS
-
-kubectl create secret generic -n monitoring grafana-admin-secret --from-literal=admin-user=${USERNAME} --from-literal=admin-password=${ADMIN_PASS} --dry-run=client -o yaml | kubeseal | yq eval -P > grafana-admin-secret.yaml
