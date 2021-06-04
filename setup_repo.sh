40f3916454c899907b2eda558084019d8745eeae
DavidSpek <vanderspek.david@gmail.com>
2021-06-04 11:00:24 +0200

    Fix setup script comment

--- setup_repo.sh
+++ setup_repo.sh
@@ -4,13 +4,13 @@
 export SETUP_CONF_PATH=$1 # location of the setup config
 export DISTRIBUTION_PATH=./distribution # folder where the distribution's YAML files are to be found
 
-# while IFS="=" read PLACEHOLDER VALUE # While loop that will perform simple parsing. On each line MY_VAR=123 will be read into PLACEHOLDER=MY_VAR, VALUE=123
-# do
-#   # recursively look for $PLACEHOLDER in all files in the $DISTRIBUTION_PATH and replace it with $VALUE
-#   echo ${VALUE}
-#   VALUE=$(echo "${VALUE////$'\/'}") #escape forward slashes (needed for sed to work correctly)
-#   grep -rli ${PLACEHOLDER} ${DISTRIBUTION_PATH}/* | xargs -i@ sed -i "s/${PLACEHOLDER}/${VALUE}/g" @ #perform recursive replace
-# done <${SETUP_CONF_PATH} # pass the setup config into the while loop
+while IFS="=" read PLACEHOLDER VALUE # While loop that will perform simple parsing. On each line MY_VAR=123 will be read into PLACEHOLDER=MY_VAR, VALUE=123
+do
+  # recursively look for $PLACEHOLDER in all files in the $DISTRIBUTION_PATH and replace it with $VALUE
+  echo ${VALUE}
+  VALUE=$(echo "${VALUE////$'\/'}") #escape forward slashes (needed for sed to work correctly)
+  grep -rli ${PLACEHOLDER} ${DISTRIBUTION_PATH}/* | xargs -i@ sed -i "s/${PLACEHOLDER}/${VALUE}/g" @ #perform recursive replace
+done <${SETUP_CONF_PATH} # pass the setup config into the while loop
 
 
 COOKIE_SECRET=$(python3 -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(16)).decode())')
