4629ebf3bbbf040ad5a87b0d786b431b36f27c99
Lars <lars@aflr.fritz.box>
2021-06-11 10:53:19 +0200

    debug changes

--- git_tag.sh
+++ git_tag.sh
@@ -3,7 +3,7 @@ echo 'push changes to git repo with tag v0.4'
 git add .
 git commit -m 'debug changes'
 git push
-git tag v0.4 --delte
+git tag v0.4 --delete
 git tag v0.4
 git push origin tag v0.4 --delete
 git push origin tag v.04
